0713d1a6832a12d406482b804888362094d667cc
Mattias Sattel <matthias.sattel@cgm.com>
2015-05-18 22:41:54 +0200

    First integration test, with server startup and teardown

--- test/wareblog/core_test.clj
+++ test/wareblog/core_test.clj
@@ -1,8 +1,27 @@
 (ns wareblog.core-test
-  (:require [clojure.test :refer :all]
-            [wareblog.core :refer :all]))
+  (require [clojure.edn :as edn :refer [read-string]]
+           [clojure.test :refer [deftest is testing use-fixtures]]
+           [wareblog.core :refer :all]
+           [org.httpkit.client :as http-client :refer [get]]))
 
 (deftest a-test
   (testing "FIXME, I fail."
     (is (= 1 1))
     (is (> 3 2))))
+
+(defn server-setup-and-teardown [f]
+  (start-server)
+  (f)
+  (stop-server))
+
+(use-fixtures :once server-setup-and-teardown)
+
+(deftest handle-a-request
+    (testing "We will do some REST API testing here"
+      (is (= 1 1))
+      (let [{:keys [status body headers] :as resp} @(http-client/get "http://localhost:3000/index.html")]
+                 (is (= (str status) "200"))
+                 (is (= (:server headers) "http-kit")))
+      ))
+
+;(edn/read-string {:readers edn-readers} (slurp (:body @(http-client/get "http://localhost:3000/articles/abc"))))
