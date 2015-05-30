ebce1c1d9cc98b9d0bdcb14da3ced8470e0f3405
Mattias Sattel <matthias.sattel@cgm.com>
2015-05-30 02:14:09 +0200

    improve edn parser for paragraph

--- src/main/clj/wareblog/articles.clj
+++ src/main/clj/wareblog/articles.clj
@@ -1,5 +1,5 @@
 (ns wareblog.articles
-  (require [clojure.edn :as edn :refer [read-string]]
+  (require [clojure.edn :as edn]
            [taoensso.timbre :as timbre]))
 
 ;Provide alias for logging with timbre
@@ -16,11 +16,14 @@
         }
    })
 
+(defn- parse-paragraph [part & more];should accept multiple args
+  (str "<p>" part more "</p>"))
+
 (def edn-readers
   {'wareblog/author str
    'wareblog/abbreviation #(str "<strong>" (:name (%1 abbreviations)) "</strong>")
    'wareblog/abstract #(reduce str %1)
-   'wareblog/paragraph #(first %1)
+   'wareblog/paragraph #(str "<p>" (reduce str %1) "</p>")
    ;'inst str
    })
 
@@ -29,7 +32,8 @@
                 :created-on #inst \"2015-05-05T21:56:00Z\"
 :created-by #wareblog/author {:given-name \"Matthias\" :name \"Sattel\"}
 :content (#wareblog/abstract (\"This is actually the very first time that I am using \" #wareblog/abbreviation :edn \". In normal situation I would simply use an existing solution for markup of a blog, because there are already good solutions out there, e.g. \" #wareblog/abbreviation :md \". But in this case I want to learn new stuff and thus I will reinvent the wheel.\")
-	 #wareblog/paragraph (\"Starting with something new is exciting, but can turn soon into frustration. Let's see how it works with \" #wareblog/abbreviation :edn))
+	 #wareblog/paragraph (\"Starting with something new is exciting, but can turn soon into frustration. Let's see how it works with \" #wareblog/abbreviation :edn)
+  #wareblog/paragraph (\"The goal of my little project is to use \" #wareblog/abbreviation :edn \" for markup. I will use it to store articles and then render it to html using custom edn-readers.\"))
 }")
 
 (defn- article-to-html [article]
